e8fb11629b69ef4046c5eff030ac99626ce897e3
davekpatrick <davekpatrick@icloud.com>
2023-01-20 19:30:31 -0500

    silent

--- node/index.js
+++ node/index.js
@@ -84,6 +84,7 @@ const setupTerraform = require('./lib/setup-terraform');
       actionSExecStdErr += data.toString();
     }
   };
+  options.silent = true;
   options.ignoreReturnCode = true;
   options.cwd = setupProduct['dirPath'];
   // Create command arguments
