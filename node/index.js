ab85970978a846137244234fc83a122bd4296e97
davekpatrick <davekpatrick@icloud.com>
2023-01-24 22:50:23 -0500

    commit and pr on write type

--- node/index.js
+++ node/index.js
@@ -4,12 +4,12 @@ const packageConfig = require('../package.json');
 // ------------------------------------
 // Node.js built-in modules
 // ------------------------------------
-
+//
 // ------------------------------------
 // External modules
 // ------------------------------------
-const actionsCore       = require('@actions/core');          // Microsoft's actions toolkit
-const github            = require('@actions/github');        // Microsoft's actions github toolkit
+const actionsCore = require('@actions/core');    // Microsoft's actions toolkit core
+const github      = require('@actions/github');  // Microsoft's actions github
 // ------------------------------------
 // Internal modules
 // ------------------------------------
@@ -129,7 +129,54 @@ const terraformFmt   = require('./lib/terraform-fmt');
   if ( terraformFmtType !== 'none' ) {
     let returnData = await terraformFmt(setupConfig['filePath'], setupConfig['dirPath'], terraformFmtType);
     actionsCore.debug('returnData[' + JSON.stringify(returnData) + ']');
-
+    // determine if we need create a commit and PR
+    if ( returnData.validFormat === false && terraformFmtType === 'write' ) {
+      const context = github.context;
+      const octokit = github.getOctokit(apiToken);
+      // commit inputs
+      const commitMessage = 'Terraform fmt';
+      const commitBranch = context.ref;
+      const commitAuthor = context.actor;
+      const commitEmail = context.actor + '@users.noreply.github.com';
+      const commitFiles = returnData.invalidFiles; 
+      // commit options
+      const commitOptions = {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        message: commitMessage,
+        branch: commitBranch,
+      };
+      if ( commitAuthor !== null && commitAuthor !== '' ) {
+        commitOptions['author'] = commitAuthor;
+      }
+      if ( commitEmail !== null && commitEmail !== '' ) {
+        commitOptions['email'] = commitEmail;
+      }
+      if ( commitFiles !== null && commitFiles !== '' ) { 
+        commitOptions['files'] = commitFiles;
+      }
+      actionsCore.debug('commitOptions[' + JSON.stringify(commitOptions) + ']');
+      // Create a commit
+      const commitResponse = await octokit.repos.createCommit(commitOptions);
+      actionsCore.debug('commitResponse[' + JSON.stringify(commitResponse) + ']');
+      actionsCore.info('commitSha[' + commitResponse.data.sha + ']')
+      // pull request options
+      const pullRequestOptions = {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        title: commitMessage,
+        head: commitBranch,
+        base: commitBranch,
+      };
+      actionsCore.debug('pullRequestOptions[' + JSON.stringify(pullRequestOptions) + ']');
+      // Create a pull request
+      const pullRequestResponse = await octokit.pulls.create(pullRequestOptions);
+      actionsCore.debug('pullRequestResponse[' + JSON.stringify(pullRequestResponse) + ']');
+      actionsCore.info('pullRequestNumber[' + pullRequestResponse.data.number + ']')
+      //
+      // Set the output
+      //actionsCore.setOutput("pullRequestNumber", pullRequestResponse.data.number);
+    }
 
   } else {
     actionsCore.info('Skipping ' + productName + ' format');
